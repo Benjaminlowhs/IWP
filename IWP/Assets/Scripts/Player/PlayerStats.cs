79598d70eade94f0bdb50e4ccfcd03949ad14a3b
Benjaminlowhs <88994519+Benjaminlowhs@users.noreply.github.com>
2023-06-03 00:19:13 +0800

    Remove inventory scripts
    Inventory is not working so removing to add new inventory system

--- IWP/Assets/Scripts/Player/PlayerStats.cs
+++ IWP/Assets/Scripts/Player/PlayerStats.cs
@@ -13,15 +13,8 @@ public class PlayerStats : MonoBehaviour
     public int resistance;
     public float speed;
     private int carryOverXp;
-    private Inventory inventory;
-    [SerializeField] private UI_Inventory uiInventory;
 
-    private void Awake()
-    {
-        inventory = new Inventory();
-        uiInventory.SetInventory(inventory);
-        ItemWorld.SpawnItemWorld(new Vector3(0,0,0), new Item{ itemType = Item.ItemType.Bandage, amount = 1});
-    }
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -45,14 +38,6 @@ public class PlayerStats : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        ItemWorld itemWorld = other.GetComponent<ItemWorld>();
-        if (itemWorld != null)
-        {
-            inventory.AddItem(itemWorld.GetItem());
-            itemWorld.DestroySelf();
-        }
-    }
+    
 
 }
